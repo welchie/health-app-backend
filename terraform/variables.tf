78ddb199ae23b692a951ee44b0f63e2d87b72bb7
Chris Welch <welchie99@gmail.com>
2026-08-31 12:15:13 +0100

    chore(infra): change default AWS region to eu-west-1

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 variable "aws_region" {
   description = "AWS region to deploy resources"
   type        = string
-  default     = "us-east-1"
+  default     = "eu-west-1"
 }
 
 variable "environment" {
